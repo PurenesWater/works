4e0897b5ccd8f09162e116575f565c9ec136ad78
administrator <administrator@wangxuedongdeMac-mini.local>
2017-07-10 14:34:07 +0800

    xxx

---
+++ wxd/WxdThre.h
@@ -0,0 +1,13 @@
+//
+//  WxdThre.h
+//  wxd
+//
+//  Created by administrator on 2017/7/10.
+//  Copyright © 2017年 XD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WxdThre : NSObject
+
+@end
---
+++ wxd/WxdThre.m
@@ -0,0 +1,13 @@
+//
+//  WxdThre.m
+//  wxd
+//
+//  Created by administrator on 2017/7/10.
+//  Copyright © 2017年 XD. All rights reserved.
+//
+
+#import "WxdThre.h"
+
+@implementation WxdThre
+
+@end
